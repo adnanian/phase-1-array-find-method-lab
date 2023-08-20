47bd2127cca0d2eebba4e537c6c730bbe9102ce8
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-08-19 19:15:13 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+// Tests 1-2
+function superbowlWin(record) {
+    const winningRecord = record.find(pastRecord => pastRecord.result === "W");
+    return (typeof winningRecord !== 'undefined') ? winningRecord.year : undefined;
+}
+
